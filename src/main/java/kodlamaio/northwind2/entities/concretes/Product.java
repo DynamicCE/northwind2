3fe73ed0137be802588b2720666f75ebc6bae8c7
Adınız <erkanmurat0632@gmail.com>
2024-03-25 13:50:33 +0300

    project updates

--- src/main/java/kodlamaio/northwind2/entities/concretes/Product.java
+++ src/main/java/kodlamaio/northwind2/entities/concretes/Product.java
@@ -14,7 +14,7 @@ class Product {
     @Column(name = "product_id")
     @Id
     @GeneratedValue
-    private int id ;
+    private int id;
     @Column(name = "category_id")
     private int categoryId;
     @Column(name = "product_name")
@@ -23,6 +23,11 @@ class Product {
     private short unitsInStock;
     @Column(name = "quantity_per_unit")
     private String quantityPerUnit;
+
+
+    public
+    Product () {
+    }
 
     public
     Product ( int id, int categoryId, String productName, short unitsInStock, String quantityPerUnit ) {
